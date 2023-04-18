9a365423b940bc9aad3fa94a74bd891c0c75ba68
M-AminAlizadeh <mohammadaminalaizadeh1378@gmail.com>
2023-04-18 12:01:16 +0330

    Solve the mobile menu link display issue

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,28 +1,21 @@
 /* eslint-disable linebreak-style */
 const mobileMenuIcon = document.querySelector('#humber-icon');
 const mobileMenu = document.querySelector('.mobile-menu');
-const  menuLink = document.querySelectorAll('.menu-link')
+const menuLink = document.querySelectorAll('.menu-link');
 const mobileMenuClose = document.querySelector('.mobile-menu-close-btn');
-
-
-
 
 mobileMenuIcon.addEventListener('click', (e) => {
   e.preventDefault();
-  mobileMenu.style.display ='flex';
-
+  mobileMenu.style.display = 'flex';
 });
 
 mobileMenuClose.addEventListener('click', (e) => {
   e.preventDefault();
   mobileMenu.style.display = 'none';
-  // mobileMenu.classList.toggle('mobile-menu-toggler');
 });
 
 menuLink.forEach((link) => {
   link.addEventListener('click', () => {
-    // mobileMenu.classList.remove('mobile-menu');
-    mobileMenu.classList.toggle('mobile-menu-toggler');
-  })
-})
-
+    mobileMenu.style.display = 'none';
+  });
+});
