03a0ee02b3e4f4969eec48b5ec013c8fad7de14f
Miki <ghermaico135@gmail.com>
2023-04-18 10:29:01 +0300

    Adding styles and javascript

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,28 @@
 /* eslint-disable linebreak-style */
 const mobileMenuIcon = document.querySelector('#humber-icon');
-const mobileMenu = document.querySelector('#mobile-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const  menuLink = document.querySelectorAll('.menu-link')
 const mobileMenuClose = document.querySelector('.mobile-menu-close-btn');
+
+
+
 
 mobileMenuIcon.addEventListener('click', (e) => {
   e.preventDefault();
-  mobileMenu.style.display = 'flex';
+  mobileMenu.style.display ='flex';
+
 });
 
 mobileMenuClose.addEventListener('click', (e) => {
   e.preventDefault();
-  mobileMenu.style.display = 'none';
+  // mobileMenu.style.display = 'none';
+  mobileMenu.classList.toggle('mobile-menu-toggler');
 });
+
+menuLink.forEach((link) => {
+  link.addEventListener('click', () => {
+    // mobileMenu.classList.remove('mobile-menu');
+    mobileMenu.classList.toggle('mobile-menu-toggler');
+  })
+})
+
