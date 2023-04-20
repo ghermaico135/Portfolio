83bd36fc9dcdf062963dc0e5e7a3c45ec77b6b2e
Miki <ghermaico135@gmail.com>
2023-04-20 17:13:54 +0300

    Adding preserve data

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,7 +3,6 @@ const mobileMenuIcon = document.querySelector('#humber-icon');
 const mobileMenu = document.querySelector('#mobile-menu');
 const mobileMenuClose = document.querySelector('.mobile-menu-close-btn');
 
-
 mobileMenuIcon.addEventListener('click', (e) => {
   e.preventDefault();
   mobileMenu.style.display = 'flex';
@@ -13,4 +12,3 @@ mobileMenuClose.addEventListener('click', (e) => {
   e.preventDefault();
   mobileMenu.style.display = 'none';
 });
-
--- js/validation-form.js
+++ js/validation-form.js
@@ -1,43 +1,36 @@
+// const userName= document.querySelector('#userName')
+// const userEmail = document.querySelector('#email');
+// const userMessage = document.querySelector('#message')
+// const submissionBtn = document.querySelector('#submit')
+// const form = document.querySelector("form")
+// const errorMessage = document.querySelector(".errorMsg");
 
-const userName= document.querySelector('#userName')
-const userEmail = document.querySelector('#email');
-const userMessage = document.querySelector('#message')
-const submissionBtn = document.querySelector('#submit')
-const form = document.querySelector("form")
-const errorMessage = document.querySelector(".errorMsg");
+// form.addEventListener('submit' ,(e) =>{
+//   if(userEmail.value !== userEmail.value.toLowerCase()){
+//     e.preventDefault();
+//     errorMessage.style.visibility='visible';
+//   }
 
+// })
 
+// // Get user data from localstorage
+// // let getUserData = { name:"", email:"", msg: ""}
 
-form.addEventListener('submit' ,(e) =>{
-  if(userEmail.value !== userEmail.value.toLowerCase()){
-    e.preventDefault();
-    errorMessage.style.visibility='visible';
-  }
-  
-})
+// window.addEventListener("load", () =>{
+//  let getUserData= JSON.parse(localStorage.getItem('setUserData'));
+//   userName.value =  getUserData.name;
+//   userEmail.value = getUserData.email;
+//   userMessage.value =  getUserData.msg;
+// })
 
+// // set userDate to localStorage
+// let setUserData= {name:"", email:"", msg: ""}
 
-// Get user data from localstorage
-// let getUserData = { name:"", email:"", msg: ""}
+// console.log(submissionBtn)
+// submissionBtn.addEventListener("click" ,() =>{
+//   setUserData.name = userName.value;
+//   setUserData.email = userEmail.value ;
+//   setUserData.msg = userMessage.value
+// localStorage.setItem('setUserData' ,JSON.stringify(setUserData))
 
-window.addEventListener("load", () =>{
- let getUserData= JSON.parse(localStorage.getItem('setUserData'));
-  userName.value =  getUserData.name;
-  userEmail.value = getUserData.email;
-  userMessage.value =  getUserData.msg;
-})
-
-// set userDate to localStorage
-let setUserData= {name:"", email:"", msg: ""}
-
-console.log(submissionBtn)
-submissionBtn.addEventListener("click" ,(e) =>{
-  // e.preventDefault();
-  setUserData.name = userName.value;
-  setUserData.email = userEmail.value ;
-  setUserData.msg = userMessage.value
-localStorage.setItem('setUserData' ,JSON.stringify(setUserData))
-
-})
-
-
+// })
