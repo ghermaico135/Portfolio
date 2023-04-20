1d1da3c69c556421b6974d5849bbdc6b8259a8c3
Miki <ghermaico135@gmail.com>
2023-04-20 16:44:37 +0300

    validation form and preserve data js

--- js/validation-form.js
+++ js/validation-form.js
@@ -1,43 +1,32 @@
-
-const userName= document.querySelector('#userName')
+const userName = document.querySelector('#userName');
 const userEmail = document.querySelector('#email');
-const userMessage = document.querySelector('#message')
-const submissionBtn = document.querySelector('#submit')
-const form = document.querySelector("form")
-const errorMessage = document.querySelector(".errorMsg");
+const userMessage = document.querySelector('#message');
+const submissionBtn = document.querySelector('#submit');
+const form = document.querySelector('form');
+const errorMessage = document.querySelector('.errorMsg');
 
-
-
-form.addEventListener('submit' ,(e) =>{
-  if(userEmail.value !== userEmail.value.toLowerCase()){
+form.addEventListener('submit', (e) => {
+  if (userEmail.value !== userEmail.value.toLowerCase()) {
     e.preventDefault();
-    errorMessage.style.visibility='visible';
+    errorMessage.style.visibility = 'visible';
   }
-  
-})
+});
 
+// Get user data from localStorage
 
-// Get user data from localstorage
-// let getUserData = { name:"", email:"", msg: ""}
-
-window.addEventListener("load", () =>{
- let getUserData= JSON.parse(localStorage.getItem('setUserData'));
-  userName.value =  getUserData.name;
+window.addEventListener('load', () => {
+  const getUserData = JSON.parse(localStorage.getItem('setUserData'));
+  userName.value = getUserData.name;
   userEmail.value = getUserData.email;
-  userMessage.value =  getUserData.msg;
-})
+  userMessage.value = getUserData.msg;
+});
 
 // set userDate to localStorage
-let setUserData= {name:"", email:"", msg: ""}
+const setUserData = { name: '', email: '', msg: '' };
 
-console.log(submissionBtn)
-submissionBtn.addEventListener("click" ,(e) =>{
-  // e.preventDefault();
+submissionBtn.addEventListener('click', () => {
   setUserData.name = userName.value;
-  setUserData.email = userEmail.value ;
-  setUserData.msg = userMessage.value
-localStorage.setItem('setUserData' ,JSON.stringify(setUserData))
-
-})
-
-
+  setUserData.email = userEmail.value;
+  setUserData.msg = userMessage.value;
+  localStorage.setItem('setUserData', JSON.stringify(setUserData));
+});
