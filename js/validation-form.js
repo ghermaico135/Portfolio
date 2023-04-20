3d349f7df1c1070302e70fabfaa6897f96774c93
Miki <ghermaico135@gmail.com>
2023-04-20 15:41:22 +0300

    Modifying validation form

--- js/validation-form.js
+++ js/validation-form.js
@@ -1,31 +1,43 @@
 
-const email = document.querySelector('#email');
+const userName= document.querySelector('#userName')
+const userEmail = document.querySelector('#email');
+const userMessage = document.querySelector('#message')
+const submissionBtn = document.querySelector('#submit')
 const form = document.querySelector("form")
 const errorMessage = document.querySelector(".errorMsg");
-// get the ids here
+
+
 
 form.addEventListener('submit' ,(e) =>{
-  e.preventDefault();
-
-  const userName = username.value;
-  const userEmail = email.value;
-  const userMsg = message.value;
-
-  if(email.value !== email.value.toLowerCase()){
+  if(userEmail.value !== userEmail.value.toLowerCase()){
+    e.preventDefault();
     errorMessage.style.visibility='visible';
-  }else{
-    console.log("supposed to be redirected")
-
   }
   
-  // storing data
-const formData = {name: userName, email: userEmail, message: userMsg}
-    
-// setting the storage and stringifying data
-localStorage.setItem('userData', JSON.stringify(formData));
-localStorage.getItem('userData', JSON.stringify(formData));
 })
 
 
+// Get user data from localstorage
+// let getUserData = { name:"", email:"", msg: ""}
+
+window.addEventListener("load", () =>{
+ let getUserData= JSON.parse(localStorage.getItem('setUserData'));
+  userName.value =  getUserData.name;
+  userEmail.value = getUserData.email;
+  userMessage.value =  getUserData.msg;
+})
+
+// set userDate to localStorage
+let setUserData= {name:"", email:"", msg: ""}
+
+console.log(submissionBtn)
+submissionBtn.addEventListener("click" ,(e) =>{
+  // e.preventDefault();
+  setUserData.name = userName.value;
+  setUserData.email = userEmail.value ;
+  setUserData.msg = userMessage.value
+localStorage.setItem('setUserData' ,JSON.stringify(setUserData))
+
+})
 
 
