c8be46f1eef0639af08f4dfad39a7f587b7d4b5b
ometman <revomet@gmail.com>
2023-04-20 10:38:32 +0000

    Add validation for email

--- js/validation-form.js
+++ js/validation-form.js
@@ -2,9 +2,14 @@
 const email = document.querySelector('#email');
 const form = document.querySelector("form")
 const errorMessage = document.querySelector(".errorMsg");
+// get the ids here
 
 form.addEventListener('submit' ,(e) =>{
   e.preventDefault();
+
+  const userName = username.value;
+  const userEmail = email.value;
+  const userMsg = message.value;
 
   if(email.value !== email.value.toLowerCase()){
     errorMessage.style.visibility='visible';
@@ -12,6 +17,15 @@ form.addEventListener('submit' ,(e) =>{
     console.log("supposed to be redirected")
 
   }
+  
+  // storing data
+const formData = {name: userName, email: userEmail, message: userMsg}
+    
+// setting the storage and stringifying data
+localStorage.setItem('userData', JSON.stringify(formData));
+localStorage.getItem('userData', JSON.stringify(formData));
 })
+
+
 
 
